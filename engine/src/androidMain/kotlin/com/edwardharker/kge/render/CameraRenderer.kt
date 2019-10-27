b96814ca528e09c70051773824ff8931ba176e53
Edward <edwardharks@yahoo.co.uk>
2019-10-27 21:47:42 +0000

    Scales the camera to fit the screen

--- engine/src/androidMain/kotlin/com/edwardharker/kge/render/CameraRenderer.kt
+++ engine/src/androidMain/kotlin/com/edwardharker/kge/render/CameraRenderer.kt
@@ -3,6 +3,10 @@ package com.edwardharker.kge.render
 import com.edwardharker.kge.component.CameraComponent
 import com.edwardharker.kge.component.TransformComponent
 import com.edwardharker.kge.util.toAndroidCanvasSpace
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import kotlin.math.min
+
 
 actual class CameraRenderer actual constructor() : Renderer {
     override var addRenderCommand: (RenderCommand) -> Unit = {}
@@ -15,6 +19,13 @@ actual class CameraRenderer actual constructor() : Renderer {
             val cameraGraphics2DPosition = cameraPosition.position.toAndroidCanvasSpace()
             val cameraHeight = 2 * camera.size
             val cameraWidth = cameraHeight * camera.aspectRatio
+
+            val widthRatio = canvas.width / cameraWidth
+            val heightRatio = canvas.height / cameraHeight
+            val ratio = min(widthRatio, heightRatio)
+
+            canvas.scale(ratio, ratio)
+
             canvas.clipRect(0f, 0f, cameraWidth, cameraHeight)
 
             canvas.translate(
